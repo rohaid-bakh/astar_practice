15374a893716be22878e51777869084d3c357cb8
Rohaid <rohaidb1999@gmail.com>
2023-08-10 14:23:35 -0400

    Added TMPro + Accounted for Grid Transform
    - Added TMPro for debugging instead of just logging
    -Took into account the grid objects rotation and position when generating a grid.
    - Added code to OnDrawGizmos that draws the lines and doesn't use the cubes.

--- Assets/Scripts/A/AStarGrid.cs
+++ Assets/Scripts/A/AStarGrid.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Assertions;
+using TMPro;
 public class AStarGrid : MonoBehaviour
 {
     //TODO: Write it so that the grid takes into account any rotations!
@@ -11,12 +12,16 @@ public class AStarGrid : MonoBehaviour
     Node[,] grid;
 
     public Transform detectableObj;
+    public Transform gridTransform;
+    public TextMeshProUGUI debug1, debug2, debug3;
 
     int gridSizeX, gridSizeY;
     public bool debug = false;
     float nodeDiameter;
 
     public void Start() {
+        Assert.IsNotNull(gridTransform);
+        Assert.IsNotNull(detectableObj);
         nodeDiameter = nodeRadius*2;
         gridSizeX = Mathf.FloorToInt(gridWorldSize.x/nodeDiameter);
         gridSizeY = Mathf.FloorToInt(gridWorldSize.y/nodeDiameter);
@@ -52,10 +57,50 @@ public class AStarGrid : MonoBehaviour
     void OnDrawGizmos()
     {
         if(debug){
+
             Gizmos.color = Color.blue;
-            // would have to rotate it according to the plane and not! to the a* script
-            // Gizmos.matrix = transform.worldToLocalMatrix;
-            Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1f, gridWorldSize.y));
+            Matrix4x4 def = Gizmos.matrix;
+            Matrix4x4 gridRotation = Matrix4x4.TRS(gridTransform.position, gridTransform.rotation, Vector3.one);
+            Gizmos.matrix = gridRotation; // so that the blue box covers the grid;
+            Gizmos.DrawWireCube(Vector3.zero, new Vector3(gridWorldSize.x, 1f, gridWorldSize.y));
+
+            Gizmos.matrix = def; // reset it 
+
+            // Gizmos.color = Color.black;
+            // Gizmos.DrawSphere(gridTransform.position, 5f);
+
+            nodeDiameter = nodeRadius*2;
+            gridSizeX = Mathf.FloorToInt(gridWorldSize.x/nodeDiameter);
+            gridSizeY = Mathf.FloorToInt(gridWorldSize.y/nodeDiameter);
+            Vector3 bottomLeft = gridTransform.position - (gridTransform.forward*(gridWorldSize.y/2)) - (gridTransform.right*(gridWorldSize.x/2));
+
+            // Gizmos.color = Color.red; 
+            // Gizmos.DrawSphere(bottomLeft, 5f);
+            
+            debug1.text = $"X offset: {gridTransform.right * (gridWorldSize.x/2)}";
+            debug2.text = $"Y offset: {gridTransform.forward * (gridWorldSize.y/2)}";
+            debug3.text = $"gridsizeX: {gridSizeX} \n gridsizeY: {gridSizeY}";
+
+            for(int row = 0; row < gridSizeX ; row ++ ){
+                for(int column = 0; column < gridSizeY; column++){            
+                    Vector3 nodeCenter = bottomLeft + (gridTransform.forward*nodeRadius+ gridTransform.forward*nodeDiameter*column ) 
+                                        + (gridTransform.right*nodeRadius + gridTransform.right*nodeDiameter*row);
+                    Vector3 BLCorner = nodeCenter - (gridTransform.forward*nodeRadius) - (gridTransform.right*nodeRadius);
+                    Vector3 ULCorner = BLCorner + (gridTransform.forward*nodeDiameter);
+                    Vector3 URCorner = ULCorner + (gridTransform.right*nodeDiameter);
+                    Vector3 BRCorner = URCorner - (gridTransform.forward*nodeDiameter);
+            
+                    Vector3[] points = new Vector3[8]{
+                        BLCorner, ULCorner,
+                        ULCorner, URCorner,
+                        URCorner, BRCorner,
+                        BRCorner, BLCorner
+                    };
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawLineList(points);
+                }
+            }
+
             Gizmos.color = Color.black;
             if(grid!= null){
                 foreach(Node n in grid){
